28f268b4abbc04bdbebd185fcb0e7d406ca397aa
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-04-17 10:23:46 +0900

    hi
    hi

---
+++ making_coding_habits_modulabs/week_2/1.Intersection of Two Linked Lists.py
@@ -0,0 +1,30 @@
+'''
+https://leetcode.com/problems/intersection-of-two-linked-lists/
+'''
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode):
+        intersectVal = 0
+        skipA, skipB = 0
+        
+        return intersectVal, skipA, skipB
+    
+
+a0 = ListNode(4)
+a1 = ListNode(1)
+a2 = ListNode(8)
+a3 = ListNode(4)
+a4 = ListNode(5)
+a0.next = a1
+a1.next = a2
+a2.next = a3
+a3.next = a4
+
+
+print(a1.next)
+
