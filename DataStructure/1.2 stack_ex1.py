0abd5432ee0b48b07db946f16c09899aa81202fc
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-04-26 14:31:36 +0900

    hi
    hi

---
+++ DataStructure/1.2 stack_ex1.py
@@ -0,0 +1,44 @@
+'''
+title : 괄호 맞추기
+input : '('와 ')'로 이루어진 문자열
+output : () 짝이 맞으면 True 
+                 맞지 않으면 False
+'''
+class Stack():
+    def __init__(self) -> None:
+        self.items = []
+        
+    def push(self,val):
+        return self.items.append(val)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def top(self):
+        return self.items[-1]
+    
+    def __len__(self):
+        return len(self.items)
+    
+stack = Stack()
+
+input = list(input("'('와')'를 입력하세요"))
+
+
+for i in input:
+    if i =='(' : 
+        stack.push(1)
+        
+    elif i ==')' :
+        stack.pop()
+
+# 이거 제너레이터로 바꿔봐라
+        
+if stack.__len__() == 0:
+    print('True')
+else:
+    print('False')
+    
+    
+    
+    
---
+++ DataStructure/test.py
@@ -0,0 +1,11 @@
+a = []
+front_idx = 0
+
+a.append(1)
+a.append(2)
+a.append(3)
+a.append(4)
+
+a.pop(0)
+a = a[1:]
+print(a)
