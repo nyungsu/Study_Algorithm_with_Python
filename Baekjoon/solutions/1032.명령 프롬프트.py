01e34e8395d39efe3ecb65bc1b33f4f9112d142b
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-03 14:02:05 +0900

    문자열 다루기
    문자열 다루기

---
+++ Baekjoon/solutions/1032.명령 프롬프트.py
@@ -0,0 +1,15 @@
+'''
+처음 입력된 문자열과 다르면 ? 집어 넣기
+'''
+
+N = int(input())
+first_word = list(input())
+first_word_len = len(first_word)
+             
+for i in range(N - 1):
+    other_words = list(input())
+    for j in range(first_word_len):
+        if first_word[j] != other_words[j]:
+            first_word[j] = '?'
+
+print(''.join(first_word))
