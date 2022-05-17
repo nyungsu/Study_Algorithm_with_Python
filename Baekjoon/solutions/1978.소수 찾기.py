9d7132a28d5a2c6071e4b3e54803c035dbbdf14f
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-17 17:41:54 +0900

    Prime number

---
+++ Baekjoon/solutions/1978.소수 찾기.py
@@ -0,0 +1,24 @@
+'''
+문제 이름 : 1978.소수 찾기
+문제 링크 : https://www.acmicpc.net/problem/1978
+시간 복잡도 : 
+공간 복잡도 : 
+'''
+
+
+n = int(input())
+
+num_list = list(map(int,input().split()))
+
+cnt = 0
+
+for i in num_list:
+    for j in range(2,i+1):
+        print(j)
+        if i%j==0:
+            if i == j:
+                cnt += 1
+            
+            
+print(cnt)
+
