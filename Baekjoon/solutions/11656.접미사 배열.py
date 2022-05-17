9eae96241d6b07ed463fc0145b8c9a3a1bfd319c
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-17 16:33:39 +0900

    String

---
+++ Baekjoon/solutions/11656.접미사 배열.py
@@ -0,0 +1,24 @@
+'''
+문제 이름 : 10867.중복 빼고 정렬하기
+문제 링크 : https://www.acmicpc.net/problem/11656
+시간 복잡도 : 
+공간 복잡도 : 
+'''
+from collections import deque
+
+str_list = input()
+result = [str_list]
+str_list = deque(str_list)
+
+
+
+for i in range(len(str_list)):
+    str_list.popleft()
+    temp = ''.join(str_list)
+    result.append(temp)
+    
+result.sort()
+result.pop(0)
+
+for i in result:
+    print(i)
