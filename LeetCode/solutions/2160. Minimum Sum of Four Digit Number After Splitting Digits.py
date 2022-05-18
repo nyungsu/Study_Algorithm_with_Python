aef9bf9f263bb052b46edb3164eda22cfecfa823
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-18 16:39:14 +0900

    sorting

---
+++ LeetCode/solutions/2160. Minimum Sum of Four Digit Number After Splitting Digits.py
@@ -0,0 +1,18 @@
+'''
+문제 이름 : 2160. Minimum Sum of Four Digit Number After Splitting Digits
+문제 링크 : https://leetcode.com/problems/minimum-sum-of-four-digit-number-after-splitting-digits/
+문제 풀이 : 와 개어이없다 이거
+시간 복잡도 : O(NlogN)
+공간 복잡도 : O(1)
+
+'''
+
+class Solution:
+    def minimumSum(self, num: int) -> int:
+        num = list(str(num))
+        num.sort()
+        num = list(map(int,num))
+
+    
+        return num[0]*10 + num[2] + num[1]*10 + num[3]
+
