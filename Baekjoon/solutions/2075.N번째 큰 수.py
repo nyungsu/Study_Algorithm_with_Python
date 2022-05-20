0cad3e1292b8f6be48ca3f03fc1cac79cebd4b30
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-20 19:54:59 +0900

    heap

--- Baekjoon/solutions/2075.N번째 큰 수.py
+++ Baekjoon/solutions/2075.N번째 큰 수.py
@@ -1,8 +1,34 @@
 '''
 문제 이름 : 2075.N번째 큰 수
 문제 링크 : https://www.acmicpc.net/problem/2075
-시간 복잡도 : 
-공간 복잡도 : 
+시간 복잡도 : O(N^2)
+공간 복잡도 : O(1)
 '''
 
-# 메모리 씨댕
+import heapq as h
+
+
+
+N = int(input())
+heap = []
+
+for _ in range(N):
+    nums = list(map(int, input().split()))
+    
+    for i in nums:
+        h.heappush(heap,i)
+        
+        if len(heap)>N:
+            h.heappop(heap)
+        
+
+print(h.heappop(heap))
+        
+    
+    
+    
+
+print(heap)
+
+
+
