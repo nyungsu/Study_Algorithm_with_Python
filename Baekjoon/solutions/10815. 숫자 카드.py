e513e34c501ddef39df4b61b994db640aeb6b01a
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-23 21:01:03 +0900

    set + in

---
+++ Baekjoon/solutions/10815. 숫자 카드.py
@@ -0,0 +1,20 @@
+'''
+문제 이름 : 10815. 숫자 카드
+문제 링크 : https://www.acmicpc.net/problem/10815
+시간 복잡도 : 
+공간 복잡도 : 
+'''
+N1 = int(input())
+target_nums = set(map(int,input().split()))
+
+N2 = int(input())
+nums = list(map(int,input().split()))
+
+result = []
+
+for i in range(max(N1,N2)):
+    if nums[i] in target_nums:
+        print(1,end=' ')
+    else :
+        print(0,end=' ')
+        
