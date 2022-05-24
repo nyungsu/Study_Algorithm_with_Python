a8418eef8ddfc128f6a878b73b2a7cc4bc668bbd
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-24 15:14:10 +0900

    못 품

--- Baekjoon/solutions/2960.에라토스테네스의 체.py
+++ Baekjoon/solutions/2960.에라토스테네스의 체.py
@@ -4,44 +4,24 @@
 시간 복잡도 : 
 공간 복잡도 : 
 '''
-from collections import deque
-import copy
+import math
 
+n = 1000 # 2부터 1,000까지의 모든 수에 대하여 소수 판별
+array = [True for i in range(n + 1)] # 처음엔 모든 수가 소수(True)인 것으로 초기화
 
-N,K = list(map(int,input().split()))
+# 에라토스테네스의 체 알고리즘 
+for i in range(2, int(math.sqrt(n)) + 1): # 2부터 n의 제곱근까지의 모든 수를 확인하며
+    if array[i] == True: # i가 소수인 경우 (남은 수인 경우)
+        # i를 제외한 i의 모든 배수를 지우기
+        j = 2 
+        while i * j <= n:
+            array[i * j] = False
+            j += 1
 
-num_list = deque(range(2,N+1))
-
-
-cnt = 0
-
-while(K != cnt):
-    P = num_list.popleft()
-
-    P_list = [P*j for j in range(len(num_list))]
-    P_list = deque(P_list)
+# 모든 소수 출력
+for i in range(2, n + 1):
+    if array[i]:
+        print(i, end=' ')
     
-    temp = P_list.popleft()
     
-    if temp in num_list:
-        cnt += 1
-        num_list.remove(temp)
-        
     
-    if K == cnt:
-        print(temp)
-        break
-
-
-print(P_list)
-            
-  
-    
-
-    
-        
-
-
-
-
-
