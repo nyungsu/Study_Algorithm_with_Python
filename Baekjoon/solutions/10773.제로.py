59148531fa507f6131ba3b0b56588da2ae4f55eb
nyungsu <81195316+nyungsu@users.noreply.github.com>
2022-05-29 16:31:10 +0900

    stack

---
+++ Baekjoon/solutions/10773.제로.py
@@ -0,0 +1,25 @@
+'''
+문제 이름 : 10773. 제로
+문제 링크 : https://www.acmicpc.net/problem/10773
+시간 복잡도 : 
+공간 복잡도 : 
+'''
+import sys
+
+N = int(input())
+
+stack = []
+
+for i in range(N):
+    num = int(sys.stdin.readline())    
+    if num == 0:
+        stack.pop()
+    
+    else :
+        stack.append(num)
+        
+print(stack)
+result = 0
+for i in stack:
+    result += i
+print(result)
